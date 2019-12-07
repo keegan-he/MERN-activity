40f582ccf9b3e1fb307d4c2db83f5b40849a1763
Keegan H <keeganslane@gmail.com>
2019-12-06 17:36:52 -0800

    users route added

--- backend/server.js
+++ backend/server.js
@@ -11,14 +11,19 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
 
 const connection = mongoose.connection;
 connection.once('open', () => {
-    console.log("MongoDB database connection established successfully");
-})
+  console.log('MongoDB database connection established successfully');
+});
 
-
+//require the files:
+const activitiesRouter = require('./routes/activities');
+const usersRouter = require('./routes/activities');
+//use the files. below - everytime a user visits the below routes it will load everything in the activities router. Same with users.
+app.use('/activities', activitiesRouter);
+app.use('users', usersRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
