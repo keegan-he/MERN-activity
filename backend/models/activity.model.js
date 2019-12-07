fd53dc9210e6e4317d1e42b3153376c3632d30fc
Keegan H <keeganslane@gmail.com>
2019-12-06 17:03:27 -0800

    activity model created

---
+++ backend/models/activity.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const activitySchema = new Schema({
+    username: { type: String, required: true },
+    description: { type: String, required: true },
+    activity_duration: { type: String, required: true },
+    date: { type: String, required: true },
+}, {
+    timestamps: true,
+});
+
+const Activity = mongoose.model('Activity', activitySchema);
+
+module.exports = Activity;
