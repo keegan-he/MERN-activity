fdcb2fe5f229e3bc55ed4be2f325949c70b2ce8e
Keegan H <keeganslane@gmail.com>
2019-12-07 19:18:12 -0800

    front end components created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Navbar from './components/navbar.component';
+import ActivityList from './components/activity-list.component';
+import EditActivity from './components/edit-activity.component';
+import CreateActivity from './components/create-activity.component';
+import CreateUser from './components/create-user.component';
 
 function App() {
   return (
-    <div className="container">
-    cleanin'
-    </div>
+    <Router>
+      <Navbar />
+      <br />
+      <div className="container">
+        <Route path="/" exact component={ActivityList} />
+        <Route path="/edit/:id" component={EditActivity} />
+        <Route path="/create" component={CreateActivity} />
+        <Route path="/user" component={CreateUser} />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/activity-list.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ActivityList extends Component {
+    render() {
+        return (
+            <div>
+            <p>Activity List component here</p>
+            </div>
+        )
+    }
+}
+
+export default ActivityList;
