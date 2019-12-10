a9254d2ee8e116b47aebc46e6cbf73e3a2943e15
Keegan H <keeganslane@gmail.com>
2019-12-09 22:04:16 -0800

    create form activity

--- src/components/create-activity.component.js
+++ src/components/create-activity.component.js
@@ -71,7 +71,63 @@ class CreateActivity extends Component {
   render() {
     return (
       <div>
-        <p>Create Activity component here</p>
+        <h3>Create Your Activity Journal</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Username: </label>
+            <select
+              ref="userInput"
+              required
+              className="form-control"
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+            >
+              {this.state.users.map(function(user) {
+                return (
+                  <option key={user} value={user}>
+                    {user}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Activity Description: </label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.activity_description}
+              onChange={this.onChangeActivityDescription}
+            />
+          </div>
+          <div className="form-group">
+            <label>Activity Duration (in minutes): </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.activity_duration}
+              onChange={this.onChangeActivityDuration}
+            />
+          </div>
+          <div className="form-group">
+            <label>Date: </label>
+            <div>
+              <DatePicker
+                selected={this.state.date}
+                onChange={this.onChangeDate}
+              />
+            </div>
+          </div>
+
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Create Activity Journal"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
       </div>
     );
   }
