38465f9a4147d11892485dc00eb6d8d719da572f
Keegan H <keeganslane@gmail.com>
2019-12-15 21:40:08 -0800

    lintin' activity.model

--- backend/models/activity.model.js
+++ backend/models/activity.model.js
@@ -2,14 +2,17 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const activitySchema = new Schema({
+const activitySchema = new Schema(
+  {
     username: { type: String, required: true },
     activity_description: { type: String, required: true },
     activity_duration: { type: Number, required: true },
-    date: { type: Date, required: true },
-}, {
-    timestamps: true,
-});
+    date: { type: Date, required: true }
+  },
+  {
+    timestamps: true
+  }
+);
 
 const Activity = mongoose.model('Activity', activitySchema);
 
